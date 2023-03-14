d7ba803a4730be8ecc0a174395e0977c0a88607c
kyleo18 <kyoliver1821@gmail.com>
2023-03-14 00:03:11 -0400

    timer/chat/enemies

---
+++ Assets/Scripts/enemyshot.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyshot : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float force;
+    [SerializeField] private float rotationForce;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Vector3 direction = target.position - rb.position;
+        direction.Normalize();
+        Vector3 rotationAmount = Vector3.Cross(transform.forward, direction);
+        rb.angularVelocity = rotationAmount * rotationForce;
+        rb.velocity = transform.forward * rotationForce;
+    }
+}
