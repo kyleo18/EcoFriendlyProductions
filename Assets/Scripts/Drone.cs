bd8836b0c0bb57072b00de61eed818a2274b3485
kyleo18 <kyoliver1821@gmail.com>
2023-03-14 00:28:56 -0400

    Revert "timer/chat/enemies"
    This reverts commit d7ba803a4730be8ecc0a174395e0977c0a88607c.

--- Assets/Scripts/Drone.cs
+++ Assets/Scripts/Drone.cs
@@ -8,11 +8,6 @@ public class Drone : MonoBehaviour
     private float thrust = 40f;
     private Rigidbody rb;
     public Transform player;
-    public GameObject dr1;
-    public GameObject dr2;
-    public GameObject dr3;
-    public GameObject dr4;
-    public GameObject dr5;
 
     private void Start()
     {
@@ -26,11 +21,7 @@ public class Drone : MonoBehaviour
         if(distance <= 30)
         {
             rb.AddRelativeForce(Vector3.forward * Mathf.Clamp((distance - 10) / 50, 0f, 1f) * thrust);
-            dr1.SetActive(true);
-            dr2.SetActive(true);
-            dr3.SetActive(true);
-            dr4.SetActive(true);
-            dr5.SetActive(true);
-        }       
+        }
+        //rb.AddRelativeForce(Vector3.forward * Mathf.Clamp((distance - 10) / 50, 0f, 1f) * thrust);
     }
 }
