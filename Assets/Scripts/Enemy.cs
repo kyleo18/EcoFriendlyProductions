91df0e54336106f534794b2c36370df84ef32972
kyleo18 <kyoliver1821@gmail.com>
2023-11-07 20:31:59 -0500

    fixed dragon

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,29 +5,52 @@ using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 public class Enemy : MonoBehaviour
 {
-    private float thrust = 10f;
+    private float thrust = 12f;
     private Rigidbody rb;
     public Transform player;
+    public Transform player2;
+    private bool dragony = true;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
     private void FixedUpdate()
-    {
-        transform.LookAt(player);
-        Vector3 targetLocation = player.position - transform.position;
-        float distance = targetLocation.magnitude;
-        rb.AddRelativeForce(Vector3.forward * Mathf.Clamp((distance - 0) / 500, 0f, 1f) * thrust);
+    {             
+        //transform.LookAt(player);
+        Vector3 targetLocation;
+        float distance;
+        if(dragony)
+        {
+            transform.LookAt(player);
+            targetLocation = player.position - transform.position;
+            //distance = targetLocation.magnitude;
+        }
+        else
+        {
+            transform.LookAt(player2);
+            targetLocation = player2.position - transform.position;
+            distance = targetLocation.magnitude;
+        }
+        distance = targetLocation.magnitude;
+        rb.AddRelativeForce(Vector3.forward * Mathf.Clamp((distance - 500) / 500, 0f, 1f) * thrust);
     }
-    private void OnCollisionEnter(Collision other)
+    //private void OnCollisionEnter(Collision other)
+    //{        
+    //    if (other.gameObject.tag == "crate")
+    //    {
+    //        Destroy(other.gameObject);
+    //    }       
+    //}
+    private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "crate")
         {
             Destroy(other.gameObject);
-        }       
-    }
-    private void OnTriggerEnter(Collider other)
-    {
+        }
+        if (other.gameObject.tag == "dragon")
+        {
+            dragony = false;
+        }
         if (other.gameObject.tag == "creaturecollider")
         {
             SceneManager.LoadScene(sceneBuildIndex: 6);
