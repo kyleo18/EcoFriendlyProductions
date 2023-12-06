11596f8537556842a28b3c8a54f04a463fa4cb01
Colton Hall <coltonmhall21@gmail.com>
2023-12-06 10:38:02 -0500

    COMPLETE LETS GO!
    Added the dialogue and a little ending thing, as well as making level 2 possible.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,7 +5,7 @@ using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 public class Enemy : MonoBehaviour
 {
-    private float thrust = 5f;
+    public float thrust = 5f;
     private Rigidbody rb;
     public Transform player;
     public Transform player2;
